8ef94403d092168a8792e4b2c59f605a461498bb
Rugved gadakh <rugvedgadakh12@gmail.com>
2022-08-27 22:44:41 +0530

    Add files via upload

---
+++ Day05/Reverse_Integer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int sum = 0;
+
+    while (n!=0){
+        int a = n%10;
+        sum = (sum*10)+a;
+
+        n = n/10;
+
+    }
+    cout<<sum<<endl;
+}
